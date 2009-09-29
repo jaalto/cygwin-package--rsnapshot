2fb32b9dabd9b7c6e2df3ecfef4e269734322125
Jari Aalto <jari.aalto@cante.net>
2009-09-30 00:37:38 +0300

    import original  CYGWIN-PATCHES 2009-09-29

---
+++ install-after.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Copyright (C) 2008 Jari Aalto; Licenced under GPL v2 or later
+#
+# install-after.sh -- Custom installation
+#
+# The script will receive one argument: relative path to
+# installation root directory. Script is called like:
+#
+#    $ install-after.sh .inst
+#
+# This script is run after [install] command. NOTE: Echo all messages
+# with ">> " prefix".
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin:/usr/X11R6/bin"
+LC_ALL="C"
+
+set -e
+
+Cmd()
+{
+    echo "$@"
+    [ "$test" ] && return
+    "$@"
+}
+
+Main()
+{
+    root=${1:-".inst"}
+
+    if [ ! "$root"  ] || [ ! -d "$root" ]; then
+        echo "$0: [ERROR] In $(pwd) no such directory: '$root'" >&2
+        return 1
+    fi
+
+    root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
+    etcdir=$root/etc
+
+    echo ">> Renaming /etc file"
+    Cmd mv $etcdir/rsnapshot.conf.default $etcdir/rsnapshot.conf
+}
+
+Main "$@"
+
+# End of file
